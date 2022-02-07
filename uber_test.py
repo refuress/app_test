d8414c7fe1dd451918f7dce6997d154e8a743e28
refuress <78370668+refuress@users.noreply.github.com>
2022-02-07 12:46:23 +0800

    Add files via upload

---
+++ uber_test.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+df = pd.read_csv("https://raw.githubusercontent.com/refuress/app_test/main/weather_hourly_darksky.csv")
+
+st.set_page_config(
+   page_title = 'Real-Time Weather Hourly Dashboard',
+   page_icon = '🔥',
+   layout = 'wide'
+)
+
+st.title("Real-Time Weather Hourly Dashboard")
+
+st.sidebar.subheader("Filter")
+
+df
+
+
+#summary = st.sidebar.multiselect(
+#    "select the final type:",
+#    options = df["summary"].unique(),
+#    default = df["summary"].unique()
+#)
+
+#df_selection = df.query("summary == @summary")
+
+#st.dataframe(df_selection)
+
